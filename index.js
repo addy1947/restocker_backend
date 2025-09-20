0b29123046b563e8364105b5ae5d3bf7dffa8faa
Aditya <adityamaurya1947@gmail.com>
2025-09-20 19:39:03 +0530

    last change i am going to do

--- index.js
+++ index.js
@@ -63,10 +63,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => {
         console.error('MongoDB connection error:', err);
